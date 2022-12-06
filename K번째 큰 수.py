f64581f5194bbf98942a7503ea73e2ef051384ff
sangjun0412 <guaba0412@gmail.com>
2022-12-06 16:15:51 +0900

    Update K번째 큰 수.py

--- K번째 큰 수.py
+++ K번째 큰 수.py
@@ -1,11 +1,11 @@
+import sys
 n, k = map(int, input().split())
 a = list(map(int, input().split()))
-a.sort(reverse= True)
-b = list()
+b = set()
 for i in range(n):
     for j in range(i + 1, n):
         for m in range(j + 1, n):
-            tmp = a[i] + a[j] + a[m]
-            b.append(tmp)
-
-print(b[k-1])
+            b.add(a[i] + a[j] + a[m])
+c = list(b)
+c.sort(reverse=True)
+print(c[k-1])
