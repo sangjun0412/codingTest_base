b4fe4427cd3d3c5d1638acd08c386802ef9fd57d
sangjun0412 <guaba0412@gmail.com>
2022-12-07 08:43:12 +0900

    Create 소수(에라토스테네스 체).py

---
+++ 소수(에라토스테네스 체).py
@@ -0,0 +1,12 @@
+n = int(input())
+ch = [0] * (n + 1)
+for i in range(2, n + 1):
+    if ch[i] == 0:
+        for j in range(i+i, n+1, i):
+            ch[j] += 1
+
+cnt = 0
+for k in range(2, n + 1):
+    if ch[k] == 0:
+        cnt += 1
+print(cnt)
