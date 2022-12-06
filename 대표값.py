6a85d9fcfc817373c15669b85c2c97984e5766e2
sangjun0412 <guaba0412@gmail.com>
2022-12-06 17:05:51 +0900

    Update 대표값.py
    round -> round_even 방식이기 때문에, +0.5 를 하고 int형 함수를 사용하여 소수 반올림을 해주는 것이 옳다.

--- 대표값.py
+++ 대표값.py
@@ -1,6 +1,6 @@
 n = int(input())
 score_list = list(map(int, input().split()))
-avg = round(sum(score_list)/n)
+avg = int(sum(score_list)/n + 0.5)
 min = 2147000000
 for idx, x in enumerate(score_list):
     tmp = abs(x - avg)
