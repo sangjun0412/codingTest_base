99b89d16e199acec39c84e1a37bf0ee1e0f469e2
sangjun0412 <guaba0412@gmail.com>
2022-12-07 09:47:29 +0900

    Create 숫자만 추출.py

---
+++ 숫자만 추출.py
@@ -0,0 +1,14 @@
+s = input()
+
+tmp = ''
+for i in s:
+    if i.isdecimal():
+        tmp += i
+tmp = int(tmp)
+cnt = 1
+for i in range(1, tmp // 2 + 1):
+    if tmp % i == 0:
+       cnt += 1
+
+print(tmp)
+print(cnt)
