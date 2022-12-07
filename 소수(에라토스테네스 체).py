d58cb0f294171c1a151347711d06d125962fa31a
sangjun0412 <guaba0412@gmail.com>
2022-12-07 09:00:01 +0900

    Update 소수(에라토스테네스 체).py

--- 소수(에라토스테네스 체).py
+++ 소수(에라토스테네스 체).py
@@ -1,6 +1,6 @@
 n = int(input())
 ch = [0] * (n + 1)
-for i in range(2, n + 1):
+for i in range(2, n + 1): #첫 소수의 배수를 전부다 체크해서, 시간초과를 막음
     if ch[i] == 0:
         for j in range(i+i, n+1, i):
             ch[j] += 1
