f51df1917e3bf694102801441a2e7d9037806e84
sangjun0412 <guaba0412@gmail.com>
2022-12-20 21:04:48 +0900

    Update (kakao)불량 사용자 .py
    주석 추가

--- (kakao)불량 사용자 .py
+++ (kakao)불량 사용자 .py
@@ -6,12 +6,12 @@ def compare(cdd, bd):
         if len(x) != len(y):
             return 0
         else:
-            for i, j in zip(x, y):
+            for i, j in zip(x, y): # candidate와 ban 리스트 비교
                 if j == '*':
                     continue
                 elif i != j:
                     return 0
-    return 1
+    return 1 # 끝까지 가면 Okay
 
 
 def solution(ud, bd):
@@ -20,10 +20,10 @@ def solution(ud, bd):
     ch = [0] * len(ud)
     res = []
 
-    def dfs(depth, cdd, size, ud, bd, ch):
+    def dfs(depth, cdd, size, ud, bd, ch): # cdd candidate, ud -> user_id , bd -> banned_id, ch -> 중복순열이 아닌 순열로
         if depth == size:
             if compare(cdd, bd):
-                res.append(cdd[:])
+                res.append(cdd[:]) # cdd로 하면, 얕은 복사가 되서, 원치 않는 값으로 마지막 복사가 됨.
         else:
             for i in range(0, len(ud)):
                 if ch[i] == 0:
@@ -34,5 +34,5 @@ def solution(ud, bd):
                     ch[i] = 0
 
     dfs(0, candidate, size, ud, bd, ch)
-    ans = set(''.join(sorted(x)) for x in res)
+    ans = set(''.join(sorted(x)) for x in res) # 정렬 후, set으로 중복되는 candidate 삭제
     return len(ans)
