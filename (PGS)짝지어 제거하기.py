05db7b91b945d6a5e28f390235d7e7b64197402b
sangjun0412 <guaba0412@gmail.com>
2023-01-04 21:01:01 +0900

    Create (PGS)짝지어 제거하기.py

---
+++ (PGS)짝지어 제거하기.py
@@ -0,0 +1,17 @@
+def solution(s):
+    answer = -1
+    s = list(map(str, s))
+    stack = []
+    for x in s:
+        if stack == []:
+            stack.append(x)
+        else:
+            if stack[-1] == x:
+                stack.pop()
+            else:
+                stack.append(x)
+
+    if stack == []:
+        return 1
+    else:
+        return 0
