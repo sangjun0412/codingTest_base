496db805a0dbe3c6ea17e1038a4a92184f3a54e9
sangjun0412 <guaba0412@gmail.com>
2023-01-04 21:01:49 +0900

    Create (PGS)구명보트.py

---
+++ (PGS)구명보트.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+
+def solution(people, limit):
+    answer = 0
+    people.sort(reverse=True)
+    q = deque(people)
+
+    while q:
+        if len(q) == 1:
+            answer += 1
+            break
+        else:
+            if q[0] + q[-1] <= limit:
+                q.popleft()
+                q.pop()
+                answer += 1
+            else:
+                q.popleft()
+                answer += 1
+
+    return answer
