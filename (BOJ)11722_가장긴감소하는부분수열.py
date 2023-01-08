12ed3f7f34b17cf0f4812cedbd3cd8298d707482
sangjun0412 <guaba0412@gmail.com>
2023-01-08 15:31:26 +0900

    Update (BOJ)11722_가장긴감소하는부분수열.py

---
+++ (BOJ)11722_가장긴감소하는부분수열.py
@@ -0,0 +1,10 @@
+n = int(input())
+nl = list(map(int, input().split()))
+dp = [1] * (n + 1)
+
+for i in range(n):
+    for j in range(i):
+        if nl[i] < nl[j]:
+            dp[i] = max(dp[i],dp[j] + 1)
+
+print(max(dp))
