83a10cba32df42863e10335616acb20754531030
sangjun0412 <guaba0412@gmail.com>
2023-01-08 15:11:57 +0900

    Update (BOJ)11055가장큰증가수열.py

--- (BOJ)11055가장큰증가수열.py
+++ (BOJ)11055가장큰증가수열.py
@@ -1,17 +1,10 @@
 n = int(input())
 nl = list(map(int, input().split()))
-dp = [0] * (n + 1)
-dp[0] = nl[0]
+dp = nl[:]
 res = 0
 
 for i in range(1, n):
-    tmp = 0
-    for j in range(i - 1, -1, -1):
-        if nl[i] > nl[j] and dp[j] > tmp:
-            tmp = dp[j]
-    dp[i] = nl[i] + tmp
-    if dp[i] > res:
-        res = dp[i]
-
-print(res)
-
+    for j in range(i):
+        if nl[i] > nl[j]:
+            dp[i] = max(dp[i], dp[j] + nl[i])
+print(max(dp))
