a944d30a869ff0dc3b8e54ebbce5bdd396e4cbbc
sangjun0412 <guaba0412@gmail.com>
2023-01-09 23:52:45 +0900

    Update (BOJ)5430_AC.py
    주석처리 참고를 해야됩니다.

--- (BOJ)5430_AC.py
+++ (BOJ)5430_AC.py
@@ -4,9 +4,9 @@ t = int(input())
 for _ in range(t):
     command = list(input())
     n = int(input())
-    nl = input()[1:-1].split(',')
+    nl = input()[1:-1].split(',') # 리스트로 들어왔을때 이부분을 주목해야됨
     nl = deque(nl)
-    flag = 0
+    flag = 0 # 그리고 q를 뒤집는게 반복된다면 flag를 활용하여 최소화로 뒤집는걸 해야됨 안그러면 시간초과가 남
     if n == 0:
         nl = deque()
     for x in command:
