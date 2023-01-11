987263802262f2aa49a4ffbc522279ac70450c60
sangjun0412 <guaba0412@gmail.com>
2023-01-11 22:28:14 +0900

    Create (PGS)할인행사.py

---
+++ (PGS)할인행사.py
@@ -0,0 +1,18 @@
+import copy
+
+
+def solution(want, number, discount):
+    answer = 0
+    user = []
+    for a, b in zip(want, number):
+        for i in range(b):
+            user.append(a)
+    user.sort()
+
+    for i in range(0, len(discount) - 9):
+        tmp = discount[i:i + 10]
+        tmp.sort()
+        if user == tmp:
+            answer += 1
+
+    return answer
