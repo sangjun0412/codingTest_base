a4d4b02338df60e0d667b0908998ab143bbc0a7e
sangjun0412 <guaba0412@gmail.com>
2023-01-13 20:00:56 +0900

    Update (BOJ)2579_계단오르기.py

--- (BOJ)2579_계단오르기.py
+++ (BOJ)2579_계단오르기.py
@@ -1,15 +1,16 @@
-n = int(input())
-nl = list()
-for i in range(n):
-    nl.append(int(input()))
-dp = [0] * (301)
+import sys
+input = sys.stdin.readline
+arr = []
+dp = []
 
+l = int(input())
+for _ in range(l):
+    arr.append(int(input()))
 
-dp[0] = 0
-dp[1] = nl[0] + nl[1]
-dp[2] = max(nl[1] + nl[2], nl[0] + nl[2])
+dp.append(arr[0])
+dp.append(max(arr[0]+arr[1],arr[1]))
+dp.append(max(arr[0]+arr[2],arr[1]+arr[2]))
+for i in range(3,l):
+    dp.append(max(dp[i-2] + arr[i] , dp[i-3] + arr[i] + arr[i - 1]))
 
-for i in range(3, n ):
-    dp[i] = max(dp[i - 3] + nl[i - 1] + nl[i], dp[i - 2] + nl[i])
-
-print(dp[n - 1])
+print(dp.pop())
