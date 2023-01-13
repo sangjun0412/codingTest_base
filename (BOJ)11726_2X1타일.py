77245f5da314252d692d820493110b12381bc349
Sangjun park <69152942+sangjun0412@users.noreply.github.com>
2023-01-13 14:26:10 +0900

    Update (BOJ)11726_2X1타일.py

--- (BOJ)11726_2X1타일.py
+++ (BOJ)11726_2X1타일.py
@@ -1,14 +1,15 @@
 """
 2*n 타일링
 dp
+범위가 적을경우 전부다 미리 dp 테이블을 설정하여야 한다!
 """
 n = int(input())
 
-dp = [0] *(n + 1)
+dp = [0] *(1002)
 
 dp[1] = 1
 dp[2] = 2
-for i in range(3, n + 1):
+for i in range(3, 1001):
     dp[i] = dp[i - 1] + dp[i - 2]
 
 print(dp[n]%10007)
