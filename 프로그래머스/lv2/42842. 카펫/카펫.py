610dadd92ba4e36499c938917c29f06655874c2d
Sangjun park <69152942+sangjun0412@users.noreply.github.com>
2023-01-17 22:22:12 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42842. 카펫/카펫.py
@@ -0,0 +1,14 @@
+def solution(brown, yellow):
+    answer = []
+    total = brown + yellow
+    candidate = []
+    for i in range(1, total // 2 + 1):
+        if not total % i:
+            if total//i >= i and i > 2:
+                candidate.append((total // i, i))
+    for x, y in candidate:
+        if (x-2) * (y-2) ==yellow:
+            return [x,y]
+            
+
+          
