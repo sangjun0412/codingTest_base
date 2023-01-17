ff4f466b825c774fccf4bfd6d311c90f4623f92b
Sangjun park <69152942+sangjun0412@users.noreply.github.com>
2023-01-17 22:25:08 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.04 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -0,0 +1,33 @@
+import heapq
+def solution(operations):
+    
+    max_heap = []
+    min_heap = []
+    heapq.heapify(max_heap)
+    heapq.heapify(min_heap)
+    
+    for x in operations:
+        op, num = map(str, x.split(" "))
+        num = int(num)
+        if op == 'I':
+            heapq.heappush(min_heap,num)
+            heapq.heappush(max_heap,-num)
+        elif op == 'D' and num == 1:
+            if len(max_heap) == 0:
+                continue
+            tmp = -heapq.heappop(max_heap)
+            min_heap.remove(tmp)
+        elif op == 'D' and num == -1:
+            if len(max_heap) == 0:
+                continue
+            tmp = heapq.heappop(min_heap)
+            max_heap.remove(-tmp)
+            
+
+    if len(max_heap) == 0:
+        answer =[0,0]
+    else:
+        answer = [max(min_heap),min(min_heap)]
+    
+
+    return answer
